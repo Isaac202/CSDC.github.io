52b19234b269473941a8d29d73d22e6c2e43624f
Isaac <isaacbyguaracity@gmail.com>
2021-07-25 10:50:30 -0300

    Subindo arquivos python

--- config/settings.py
+++ config/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media_root')
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
